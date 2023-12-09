0cf794c3445c3ecf249bdec710d67cddbc22780b
endulum <71853916+endulum@users.noreply.github.com>
2023-12-08 19:59:35 -0800

    nicer border gradient

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # polkit-kde-agent for authentication agent
 # qt5-wayland and qt6-wayland for qt wayland support
 
-sudo pacman -S hyprland xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland hyprpaper waybar alacritty flatpak brightnessctl vim github-cli swayidle swaylock ttf-freefont ttf-liberation
+sudo pacman -S hyprland xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland hyprpaper waybar alacritty flatpak brightnessctl vim github-cli swayidle swaylock ttf-freefont ttf-liberation tlp
 sudo flatpak install codium librewolf 
 
 # configure fonts
