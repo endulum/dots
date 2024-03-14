da2962742d99af79f46e0ffaa5be58bb2380f17c
endulum <71853916+endulum@users.noreply.github.com>
2024-03-14 11:37:32 -0700

    change

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # polkit-kde-agent for authentication agent
 # qt5-wayland and qt6-wayland for qt wayland support
 
-sudo pacman -S hyprland xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland hyprpaper waybar alacritty flatpak brightnessctl vim github-cli swayidle swaylock ttf-freefont ttf-liberation tlp
+sudo pacman -S hyprland xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland hyprpaper waybar alacritty flatpak brightnessctl vim github-cli swayidle swaylock ttf-freefont ttf-liberation tlp mako feh flameshot mousepad pavucontrol pulseaudio wofi
 sudo flatpak install codium librewolf 
 
 # configure fonts
@@ -23,7 +23,7 @@ mkdir ~/.config/waybar
 ln -s ~/dots/waybar-config ~/.config/waybar/config
 ln -s ~/dots/waybar-style ~/.config/waybar/style.css
 # configure alacritty
-ln -s ~/dots/alacritty-config ~/.alacritty.yml
+ln -s ~/dots/alacritty-config ~/.alacritty.toml
 # echo "alias split='alacritty --working-directory $(pwd) & disown'" >> ~/.bashrc
 # configure codium
 rm /home/endulum/.var/app/com.vscodium.codium/config/VSCodium/User/settings.json
